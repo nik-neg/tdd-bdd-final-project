b6f56c0dcaee1d851fd0c93854768c83d6502a3b
alex <alex.genkin.13@gmail.com>
2025-07-13 12:23:55 +0300

    add test product not found by id

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -168,8 +168,35 @@ class TestProductRoutes(TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
-    def test_product_not_found_by_id():
-        pass
+    def test_product_not_found_by_id(self):
+        """It should not find a product that does not exist and return 404"""
+        test_product = ProductFactory()
+        logging.debug("Test Product: %s", test_product.serialize())
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = response.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_product = response.get_json()
+        self.assertEqual(new_product["name"], test_product.name)
+        self.assertEqual(new_product["description"], test_product.description)
+        self.assertEqual(Decimal(new_product["price"]), test_product.price)
+        self.assertEqual(new_product["available"], test_product.available)
+        self.assertEqual(new_product["category"], test_product.category.name)
+
+        #
+        # Uncomment this code once READ is implemented
+        #
+
+        # Check that the location header was correct
+        # logging.debug("Location: %s", location)
+        
+        id = new_product['id'] + 1
+        response = self.client.get(f'/products/{id}')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     ######################################################################
     # Utility functions
